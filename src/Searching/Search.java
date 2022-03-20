2f1c20f619f59c73a497dbc8f325ab5b4fd22869
tpstjn <63107315+tpstjn@users.noreply.github.com>
2022-03-20 15:21:25 -0400

    Searching 1.0

--- src/Searching/Search.java
+++ src/Searching/Search.java
@@ -1,25 +1,37 @@
 package Searching;
 
-import java.io.File;
+//region Imports
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.Iterator;
+import java.text.SimpleDateFormat;
 
-import org.apache.poi.ss.formula.functions.Column;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+//endregion
+
+/**
+ * Represents a search being performed by the user
+ */
 public class Search {
+
+    //region Member Variables
+
     XSSFWorkbook courses = null;
     XSSFSheet sheet = null;
 
-    private final int CODE_COL = 0;
-    private final int TITLE_LONG_COL = 2;
-    private final int START_COL = 3;
-    private final int END_COL = 4;
-    private final int DAYS_COL = 5;
+    //endregion
+
+    //region Search()
 
     /**
      * Initializes search class by opening and preparing spreadsheet for search
@@ -35,8 +47,13 @@ public class Search {
         catch(IOException io) {
             System.out.println("The specified document could not be converted (is it a .xslx?).");
         }
+        //We just have one sheet, so grab the first one
         sheet = courses.getSheetAt(0);
     }
+
+    //endregion
+
+    //region getResults()
 
     /**
      * Gets results of a search of GCC courses based on search parameters provided by user
@@ -44,31 +61,167 @@ public class Search {
      * @return String displaying search results
      */
     public String getResults(SearchParameter searchParams) {
+
+        //region __init__
+
+        final int COL_CODE = 0;
+        final int COL_SHORT_TITLE = 1;
+        final int COL_TITLE = 2;
+        final int COL_START = 3;
+        final int COL_END = 4;
+        final int COL_DAYS = 5;
+        final int COL_BUILDING = 6;
+        final int COL_ROOM = 7;
+        final int COL_ENROLLMENT = 8;
+        final int COL_CAPACITY = 9;
+
+        SimpleDateFormat formatTime = new SimpleDateFormat("HH:mm");
         StringBuilder value = new StringBuilder();
+        Formatter formatter = new Formatter(value);
         String input = searchParams.getValue();
 
+        boolean skipHeader = true;
+        ArrayList<Row> matchingRows = new ArrayList<>();
+        for(Row row : sheet) {
+            if (skipHeader) {
+                skipHeader = false;
+                continue;
+            }
+            matchingRows.add(row);
+        }
+
+        int nextInput = 0;
+
+        //endregion
+
+        //region Filter search by Course Code
+
         if(searchParams.getFlags().contains(SearchParameter.Flag.CODE)) {
-//            Row row = sheet.getRow(0); //returns the logical row
-            String code = input.split("\n")[0];
-            System.out.println(code);
-            for(Row row : sheet) {
-                Cell cell = row.getCell(CODE_COL);
-//                System.out.println(cell.toString());
-                if(cell.getStringCellValue().contains(code)) {
-                    value.append(cell.getStringCellValue()).append("\n");
+            //Assumes that user input is being passed in as a new-line delineated string
+            String code = input.split("\n")[nextInput];
+            nextInput++;
+
+            //Iterate through ArrayList using iterator so we can remove from it
+            for (Iterator<Row> it = matchingRows.iterator(); it.hasNext(); ) {
+                Row row = it.next();
+                Cell courseCode = row.getCell(COL_CODE);
+
+                //If the codes don't match, remove that row
+                if(!(courseCode.getStringCellValue().contains(code))) {
+                    it.remove();
                 }
             }
-//            Cell cell=row.getCell(0); //getting the cell representing the given column
-//            value = cell.getStringCellValue();
         }
+
+        //endregion
+
+        //region Next, filter by time
+
+        if(searchParams.getFlags().contains(SearchParameter.Flag.TIME)) {
+
+            //Assumes start time & end time are new line delineated, in 24hr format
+            //TODO: Ask - will they always enter start & finish time or can they enter just start or just finish
+            LocalTime start_in = LocalTime.parse(input.split("\n")[nextInput]);
+            nextInput++;
+            LocalTime end_in = LocalTime.parse(input.split("\n")[nextInput]);
+            nextInput++;
+
+            for (Iterator<Row> it = matchingRows.iterator(); it.hasNext(); ) {
+                Row row = it.next();
+                Cell startTime = row.getCell(COL_START);
+                Cell endTime = row.getCell(COL_END);
+
+                if(startTime.getCellTypeEnum().equals(CellType.NUMERIC) && endTime.getCellTypeEnum().equals(CellType.NUMERIC)) {
+                    LocalTime start = LocalTime.parse(formatTime.format(startTime.getDateCellValue()));
+                    LocalTime finish = LocalTime.parse(formatTime.format(endTime.getDateCellValue()));
+
+                    if(start_in.isAfter(start) || end_in.isBefore(finish)) {
+                        it.remove();
+                    }
+                }
+                //Remove the class from consideration if the time is NULL
+                else {
+                    it.remove();
+                }
+            }
+        }
+
+        //endregion
+
+        //region Next, filter by meeting days
+
+        if(searchParams.getFlags().contains(SearchParameter.Flag.DATE)) {
+            String meetDays_in = input.split("\n")[nextInput];
+            nextInput++;
+
+
+            for (Iterator<Row> it = matchingRows.iterator(); it.hasNext(); ) {
+                Row row = it.next();
+                Cell meetDays = row.getCell(COL_DAYS);
+
+                if(!meetDays.getStringCellValue().contains(meetDays_in)) {
+                    it.remove();
+                }
+            }
+        }
+
+        //endregion
+
+        //region Next, filter by course name
+
+        if(searchParams.getFlags().contains(SearchParameter.Flag.NAME)) {
+            String name_in = input.split("\n")[nextInput].toUpperCase();
+
+            for (Iterator<Row> it = matchingRows.iterator(); it.hasNext(); ) {
+                Row row = it.next();
+                Cell name = row.getCell(COL_TITLE);
+
+                if(!name.getStringCellValue().contains(name_in)) {
+                    it.remove();
+                }
+            }
+        }
+
+        //endregion
+
+        //region String building
+
+        for(Row row : matchingRows) {
+            String code = row.getCell(COL_CODE).getStringCellValue();
+            String title = row.getCell(COL_SHORT_TITLE).getStringCellValue();
+            String start = "N/A";
+            if(row.getCell(COL_START).getCellTypeEnum().equals(CellType.NUMERIC))
+                start = formatTime.format(row.getCell(COL_START).getDateCellValue());
+            String end = "N/A";
+            if(row.getCell(COL_END).getCellTypeEnum().equals(CellType.NUMERIC))
+                end = formatTime.format(row.getCell(COL_END).getDateCellValue());
+            String days = row.getCell(COL_DAYS).getStringCellValue();
+            String building = row.getCell(COL_BUILDING).getStringCellValue();
+            if(building.equals("NULL")) {
+                building = "TBD";
+            }
+            String room = "";
+            if(row.getCell(COL_ROOM).toString().contains(".")) {
+                room = row.getCell(COL_ROOM).toString().split("\\.")[0];
+            }
+            int enrollment = (int)row.getCell(COL_ENROLLMENT).getNumericCellValue();
+            int capacity = (int)row.getCell(COL_CAPACITY).getNumericCellValue();
+
+            formatter.format("%s %18s %3s %5s %s %5s %5s %4s %d%s%d\n", code, title, days, start, "-", end,
+                    building, room, enrollment, "/", capacity);
+        }
+
+        //endregion
 
         return value.toString();
     }
 
+    //endregion
+
     public static void main(String[] args) {
         Search search = new Search();
-        String input = "COMP\n";
-        SearchParameter param = new SearchParameter(true, true, true, input);
+        String input = "COMP";
+        SearchParameter param = new SearchParameter(true, false, false, false, input);
         System.out.println(search.getResults(param));
     }
 }
--- src/Searching/SearchParameter.java
+++ src/Searching/SearchParameter.java
@@ -2,19 +2,40 @@ package Searching;
 
 import java.util.EnumSet;
 
+/**
+ * Represents all search filters/queries passed in by user
+ */
 public class SearchParameter {
+
+    //region Member Variables
+
+    /**
+     * Flags for various search filters
+     */
     public enum Flag {
         CODE,
         NAME,
-        DATETIME
+        DATE,
+        TIME
     }
     private EnumSet<Flag> flags = EnumSet.noneOf(Flag.class);
     private String value;
 
-    public SearchParameter(boolean byCourseCode, boolean byName, boolean byDateTime, String value) {
+    //endregion
+
+    /**
+     * Used in Search class to specify what user is searching for
+     * @param byCourseCode Determines if user is searching by course code
+     * @param byTime Determines if user is searching by time
+     * @param byDate Determines if user is searching by date
+     * @param byName Determines if user is searching by course name
+     * @param value Specifies values entered by user, each value should be new line delimited
+     */
+    public SearchParameter(boolean byCourseCode, boolean byTime, boolean byDate, boolean byName, String value) {
         if(byCourseCode) flags.add(Flag.CODE);
         if(byName) flags.add(Flag.NAME);
-        if(byDateTime) flags.add(Flag.DATETIME);
+        if(byDate) flags.add(Flag.DATE);
+        if(byTime) flags.add(Flag.TIME);
         this.value = value;
     }
 

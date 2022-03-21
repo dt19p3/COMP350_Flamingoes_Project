5e1dae927d6e3f2958b63d126b95d2f4d6558eff
amcfeaters7 <mcfeatersaj19@gcc.edu>
2022-03-21 14:29:22 -0400

    Add Course class

--- src/Course.java
+++ src/Course.java
@@ -1,5 +1,24 @@
-public class Course {
-    public Course(){
+import java.time.LocalTime;
 
+public class Course {
+    String code;
+    String shortTitle;
+    String longTitle;
+    LocalTime beginTime;
+    LocalTime endTime;
+    String meets;
+    String building;
+    String room;
+
+    public Course (String code,  String shortTitle, String longTitle, LocalTime beginTime, LocalTime endTime,
+                   String meets, String building, String room){
+        this.code = code;
+        this.shortTitle = shortTitle;
+        this.longTitle = longTitle;
+        this.beginTime = beginTime;
+        this.endTime = endTime;
+        this.meets = meets;
+        this.building = building;
+        this.room = room;
     }
 }

263a512861c7ffced95fceec2e8b5691a9af32a6
TERZANODJ19 <terzanodj19@gcc.edu>
2022-03-22 17:15:53 -0400

    Added a constructor that uses all fields in Class diagram, but the other constructor will still work, added default values for it. Added getters and setters for everything

--- src/Course.java
+++ src/Course.java
@@ -1,10 +1,11 @@
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Formatter;
 
 public class Course {
     String code;
-    String shortTitle;
-    String longTitle;
+    String shortTitle; //name in class diagram
+    String longTitle; //description in class diagram
     LocalTime beginTime;
     LocalTime endTime;
     String meets;
@@ -12,19 +13,176 @@ public class Course {
     String room;
     int enrollment;
     int capacity;
+    int numCredits;
+    String professor;
+    String department;
+    String semester;
+    ArrayList<Course> labs;
+    ArrayList<Course> prereqs;
 
+
+
+
+
+    // this is a constructor that uses all fields in the database
     public Course (String code,  String shortTitle, String longTitle, LocalTime beginTime, LocalTime endTime,
                    String meets, String building, String room, int enrollment, int capacity){
+        this(code,  shortTitle, longTitle,beginTime, endTime, meets, building, room, enrollment, capacity,
+                0, "", "", "", new ArrayList<>(), new ArrayList<>());
+
+    }
+
+   // this is a constructor that includes the fields in our class diagram, expected to use this when reading
+    // schedule courses saved to file back into memory
+   public Course (String code,  String shortTitle, String longTitle, LocalTime beginTime, LocalTime endTime,
+                  String meets, String building, String room, int enrollment, int capacity, int numCredits,
+                  String professor, String department, String semester, ArrayList<Course> labs,
+                  ArrayList<Course> prereqs) {
+       this.code = code;
+       this.shortTitle = shortTitle;
+       this.longTitle = longTitle;
+       this.beginTime = beginTime;
+       this.endTime = endTime;
+       this.meets = meets;
+       this.building = building;
+       this.room = room;
+       this.enrollment = enrollment;
+       this.capacity = capacity;
+       this.numCredits = numCredits;
+       this.professor = professor;
+       this.department = department;
+       this.semester = semester;
+       this.labs = labs;
+       this.prereqs = prereqs;
+   }
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
         this.code = code;
+    }
+
+    public String getShortTitle() {
+        return shortTitle;
+    }
+
+    public void setShortTitle(String shortTitle) {
         this.shortTitle = shortTitle;
+    }
+
+    public String getLongTitle() {
+        return longTitle;
+    }
+
+    public void setLongTitle(String longTitle) {
         this.longTitle = longTitle;
+    }
+
+    public LocalTime getBeginTime() {
+        return beginTime;
+    }
+
+    public void setBeginTime(LocalTime beginTime) {
         this.beginTime = beginTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
         this.endTime = endTime;
+    }
+
+    public String getMeets() {
+        return meets;
+    }
+
+    public void setMeets(String meets) {
         this.meets = meets;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
         this.building = building;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
         this.room = room;
+    }
+
+    public int getEnrollment() {
+        return enrollment;
+    }
+
+    public void setEnrollment(int enrollment) {
         this.enrollment = enrollment;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
         this.capacity = capacity;
+    }
+
+    public int getNumCredits() {
+        return numCredits;
+    }
+
+    public void setNumCredits(int numCredits) {
+        this.numCredits = numCredits;
+    }
+
+    public String getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(String professor) {
+        this.professor = professor;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public ArrayList<Course> getLabs() {
+        return labs;
+    }
+
+    public void setLabs(ArrayList<Course> labs) {
+        this.labs = labs;
+    }
+
+    public ArrayList<Course> getPrereqs() {
+        return prereqs;
+    }
+
+    public void setPrereqs(ArrayList<Course> prereqs) {
+        this.prereqs = prereqs;
     }
 
     @Override
